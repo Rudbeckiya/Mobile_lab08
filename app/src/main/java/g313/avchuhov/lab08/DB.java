6effe03ddb768b329bc0dd2ac9459ade258546c3
Vlad <Vlad.avchukhov@mail.ru>
2024-02-05 13:32:01 +0300

    lab08

--- app/src/main/java/g313/avchuhov/lab08/DB.java
+++ app/src/main/java/g313/avchuhov/lab08/DB.java
@@ -1,25 +1,48 @@
 package g313.avchuhov.lab08;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class DB extends SQLiteOpenHelper
 {
-
-    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version)
+    {
         super(context, name, factory, version);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase db)
+    {
+        String sql = "CREATE TABLE notes (id INT, txt TEXT);";
+        db.execSQL(sql);
+    }
 
+    public int getMaxId()
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = "SELECT MAX(id) FROM notes;";
+        Cursor cur = db.rawQuery(sql, null);
+        if (cur.moveToFirst() == true) return cur.getInt(0);
+        return 0;
+    }
+
+    public void addNote(int id, String stxt)
+    {
+        String sid = String.valueOf(id);
+        SQLiteDatabase db = getWritableDatabase();
+        String sql = "INSERT INTO notes VALUES (" + sid + ", '" + stxt + "');";
+        db.execSQL(sql);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
 
     }
 }
