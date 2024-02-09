cb0967abe799568615b96876e743732de83717c1
Vlad <Vlad.avchukhov@mail.ru>
2024-02-09 17:33:21 +0300

    lab08

--- app/src/main/java/g313/avchuhov/lab08/DB.java
+++ app/src/main/java/g313/avchuhov/lab08/DB.java
@@ -40,6 +40,34 @@ public class DB extends SQLiteOpenHelper
         db.execSQL(sql);
     }
 
+    public String getNote(int id)
+    {
+        String sid = String.valueOf(id);
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = "SELECT txt FROM notes WHERE id = " + sid + ";";
+        Cursor cur = db.rawQuery(sql, null);
+        if (cur.moveToFirst() == true) return cur.getString(0);
+        return "";
+    }
+
+    public void getAllNotes(ArrayList<mynote> lst)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = "SELECT id, txt FROM notes;";
+        Cursor cur = db.rawQuery(sql, null);
+        if (cur.moveToFirst() == true) {
+            do {
+                mynote n = new mynote();
+                n.id = cur.getInt(0);
+                n.txt = cur.getString(1);
+                lst.add(n);
+            } while (cur.moveToNext() == true);
+        }
+    }
+
+    // TODO: add "alterNote" function to save edited note
+    // TODO: add "deleteNote" function to delete existing note
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
--- app/src/main/java/g313/avchuhov/lab08/MainActivity.java
+++ app/src/main/java/g313/avchuhov/lab08/MainActivity.java
@@ -1,14 +1,56 @@
 package g313.avchuhov.lab08;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    ListView lstctl;
+    ArrayList<mynote> lst = new ArrayList <> ();
+    ArrayList<mynote> adp;
+
+    Context ctx;
+
+    void update_list()
+    {
+        lst.clear();
+        g.notes.getAllNotes(lst);
+        adp.notifyDataSetChanged();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        update_list();
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ctx = this;
+
+        g.notes = new DB(this, "notes.db", null, 1);
+
+        lstctl = findViewById(R.id.list_note);
+        adp = new ArrayAdapter <mynote> (this, android.R.layout.simple_list_item_1, lst);
+        lstctl.setAdapter(adp);
+
+        lstctl,setOnItemClickListener((parent, view, position, id) {
+            mynote n = adp.getItem(position);
+            Intent i = new Intent(ctx, Main2Activity.class);
+            DOPISHI
+        })
+
     }
 }
