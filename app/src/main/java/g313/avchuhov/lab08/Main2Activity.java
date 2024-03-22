35e5591c8c4f597ac57902e139d42dda7f8681c1
Vlad <Vlad.avchukhov@mail.ru>
2024-03-22 15:34:26 +0300

    Lab08

--- app/src/main/java/g313/avchuhov/lab08/Main2Activity.java
+++ app/src/main/java/g313/avchuhov/lab08/Main2Activity.java
@@ -1,14 +1,58 @@
 package g313.avchuhov.lab08;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
+    EditText txtctl;
+
+    int nid;
+    String ntxt;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        txtctl = findViewById(R.id.txt_content);
+
+        Intent i = getIntent();
+        nid = i.getIntExtra("note-id", 0);
+        ntxt = i.getStringExtra("note-txt");
+
+        txtctl.setText(ntxt);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.item_save)
+        {
+            // TODO: get text from text box, modify note, show toast "note saved" and exit
+        }
+        else if (id == R.id.item_delete)
+        {
+            // TODO: alert dialog with yes/no confirmation
+            // TODO: delete note, show toast "note deleted" and exit ativity
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/g313/avchuhov/lab08/MainActivity.java
+++ app/src/main/java/g313/avchuhov/lab08/MainActivity.java
@@ -1,11 +1,14 @@
 package g313.avchuhov.lab08;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -46,15 +49,40 @@ public class MainActivity extends AppCompatActivity
         adp = new ArrayAdapter <mynote> (this, android.R.layout.simple_list_item_1, lst);
         lstctl.setAdapter(adp);
 
-        lstctl.setOnItemClickListener((parent, view, position, id) -> {
-
-        mynote n = adp.getItem(position);
-        Intent i = new Intent(ctx, Main2Activity.class);
-        i.putExtra("note-id", n.id);
-        i.putExtra("note-txt", n.txt);
-        startActivityForResult(i, 1);
-    });
+        lstctl.setOnItemClickListener((parent, view, position, id) ->
+        {
+            mynote n = adp.getItem(position);
+            Intent i = new Intent(ctx, Main2Activity.class);
+            i.putExtra("note-id", n.id);
+            i.putExtra("note-txt", n.txt);
+            startActivityForResult(i, 1);
+        });
 
         update_list();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.item_new)
+        {
+            int nid = g.notes.getMaxId() + 1;
+            g.notes.addNote(nid, "Hello, world!");
+            update_list();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
